d69b0815c050132ab1de7440290530fe0af8b016
Jdubstep1357 <jwlyons2015@gmail.com>
2024-02-05 17:05:43 -0600

    Double clicking on new entry allows one to change input text

---
+++ js/todos.js
@@ -0,0 +1,48 @@
+window.todos = function () {
+    return {
+    
+        todos: [],
+
+        editedTodo: null,
+
+
+        get active() {
+            return this.todos.filter(todo => ! todo.completed);
+        },
+
+        get completed() {
+            return this.todos.filter(todo => todo.completed);
+        },
+
+        newTodo: '',
+
+
+        addTodo() {
+            this.todos.push({
+                id: this.todos.length + 1,
+                body: this.newTodo,
+                completed: false
+            });
+
+            this.newTodo = '';
+        },
+
+        editTodo(todo) {
+            this.editedTodo = todo;
+        },
+
+        // After person double clicks and changes selection, confirm it
+        editComplete(todo) {
+            this.editedTodo = null;
+        },
+
+        deleteTodo(todo) {
+            let position = this.todos.indexOf(todo);
+
+            this.todos.splice(position, 1);
+        },
+        
+    }
+
+
+}
